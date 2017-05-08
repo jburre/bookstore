bebf1521e201b2344d86a0cf3d7115b32dfec506
jburre <Jan.H.Burre@gmail.com>
2017-05-08 11:46:42 +0200

    feat: some additional styling
    added some placeholder images

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	@RequestMapping(value={"","/","/index"})
-	@ResponseBody
 	public String index(){
 		return "index";
 	}
