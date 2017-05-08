b2387c48c1b96da2d9a191d24a4a3e335d386388
jburre <Jan.H.Burre@gmail.com>
2017-05-08 10:11:14 +0200

    feat: some basic styles and mapping

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping(value={"","/","/index"})
+	@ResponseBody
 	public String index(){
 		return "index";
 	}
